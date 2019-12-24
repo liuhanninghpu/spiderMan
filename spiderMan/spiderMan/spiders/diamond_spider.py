e47c863f587eab59d1d630aab011bea67bb15f28
matt.liu <469057420@qq.com>
2019-12-24 22:53:39 +0800

    bugfixed

--- spiderMan/spiderMan/spiders/diamond_spider.py
+++ spiderMan/spiderMan/spiders/diamond_spider.py
@@ -1,18 +1,17 @@
 import scrapy
 from ..items import FilmsCityItem
-from scrapy import Request
-import Crypto.PublicKey.RSA as RSA
+import rsa
+import binascii
+from scrapy.http.cookies import CookieJar
 
-
-
-#注意使用Crypto需要安装vs C++ 14 build tools
 
 class DiamondSpider(scrapy.Spider):
     name = "diamond"
     allowed_domains = ['www.zzhgia.com']
     start_urls = ['http://www.zzhgia.com/']
     base_url = 'http://www.zzhgia.com/'
-    load_url = 'http://www.zzhgia.com/login/pc/dologin_manager_specialty.xhtml'
+    #一些操作的url
+    login_url = 'http://www.zzhgia.com/login/pc/dologin_manager_specialty.xhtml'
 
     #登录系统的帐号密码
     load_info = {
@@ -21,18 +20,26 @@ class DiamondSpider(scrapy.Spider):
         'www':'www.zzhgia.com'
     }
 
-    #rsa公钥
-    rsa_public_key = '9451cf74bef83e6d0192bc3d533177c3353613627e2ec4926355ca88e7b015a656257f9afaa0172a82e9bb01f4e9e3d096fdb82c7936cb72ffc63926bfbe64100b9f341046112b7cbea8e4e5ec74165172afe283fb4b122b23ad7b0a309cb6cce1edea220657c013b42197f2348965c028e22faa88c01d29989b9aa38e896c79'
-
+    #rsa
+    #模数和指数
+    modulus = '9451cf74bef83e6d0192bc3d533177c3353613627e2ec4926355ca88e7b015a656257f9afaa0172a82e9bb01f4e9e3d096fdb82c7936cb72ffc63926bfbe64100b9f341046112b7cbea8e4e5ec74165172afe283fb4b122b23ad7b0a309cb6cce1edea220657c013b42197f2348965c028e22faa88c01d29989b9aa38e896c79'
+    exponent = '10001'
 
     def start_requests(self):
-        e = int('10001', 16)
-        n = int(self.rsa_public_key, 16)  # snipped for brevity
-        pubkey = RSA.construct((n, e))
-        code = pubkey.encrypt(self.load_info['password'], None)
-        print(code)
+        modulus = int(self.modulus,16)
+        exponent = int(self.exponent,16)
+        public_key = rsa.PublicKey(modulus,exponent)
+        password_rsa_code = binascii.b2a_hex(rsa.encrypt(self.load_info['password'].encode(),public_key))#加密信息转换成16进制
+        self.load_info['password'] = password_rsa_code
+        return [scrapy.FormRequest(self.login_url,formdata=self.load_info,callback=self.after_login)]
+
+
+    def after_login(self,response):
+        print(response.session)
+        print(response.body)
+        scrapy.Request(self.url_somebody, dont_filter=True, meta={'proxy': 'http://127.0.0.1:8888'})
         exit()
-        request = scrapy.FormRequest(self.load_url,formdata=self.load_info)
+        pass
 
     def parse(self, response):
         item = FilmsCityItem()
